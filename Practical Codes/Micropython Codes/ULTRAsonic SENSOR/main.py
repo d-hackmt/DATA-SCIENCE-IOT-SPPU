024fb4ef7393a1e6d949b50d1e314ec8bc1f153e
Divesh Jadhwani <113240252+d-hackmt@users.noreply.github.com>
2025-09-25 08:47:58 +0530

    Update main.py

--- Practical Codes/Micropython Codes/ULTRAsonic SENSOR/main.py
+++ Practical Codes/Micropython Codes/ULTRAsonic SENSOR/main.py
@@ -1,9 +1,55 @@
+# import machine
+# import time
+
+# trig = machine.Pin(12, machine.Pin.OUT)
+# echo = machine.Pin(14, machine.Pin.IN)
+# led = machine.Pin(2, machine.Pin.OUT)
+
+# # Set the threshold distance in centimeters
+# threshold_distance = 100.0  # Change this value as needed
+
+# while True:
+    
+#     trig.on()
+#     time.sleep_us(10)
+#     trig.off()
+
+#     while not echo.value():
+#         pass
+#     start_time = time.ticks_us()
+
+#     while echo.value():
+#         pass
+#     end_time = time.ticks_us()
+
+#     pulse_duration = time.ticks_diff(end_time, start_time)
+#     distance = pulse_duration * 0.0343 / 2
+#     print("Distance: {:.2f} cm".format(distance))
+
+#     if distance < threshold_distance:
+#         led.on()
+#     else:
+#         led.off()
+
+#     time.sleep(1)
+
+
+
+from hcsr04 import HCSR04
 import machine
-from machine import RTC
+from machine import Pin
+from time import sleep
 
-# Initialize the RTC module
-rtc = RTC()
+led = Pin(26 , Pin.OUT)
+# ESP32
 
-# Read and print the current date and time
-current_datetime = rtc.datetime()
-print("Current Date and Time:", current_datetime)
+sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
+ 
+while True:
+    distance = sensor.distance_cm()
+    print('Distance:', distance, 'cm')
+    if distance < 50:
+        led.on()
+    else:
+        led.off()
+    sleep(1)
